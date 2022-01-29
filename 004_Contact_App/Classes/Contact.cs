3e3a34340ddf39a9ebfae4f65f898c5b93f86572
Wellington Pereira da Costa <well_pcosta@hotmail.com>
2022-01-28 22:43:45 -0300

    Cration of Contact class

---
+++ 004_Contact_App/Classes/Contact.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _004_Contact_App.Classes
+{
+    public class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
