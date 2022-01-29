c510a473a60c8cb116e0e2138d1095e916a15649
Wellington Pereira da Costa <well_pcosta@hotmail.com>
2022-01-28 23:11:50 -0300

    Creation of Insert (Click on save button)

--- 004_Contact_App/NewContactWindow.xaml.cs
+++ 004_Contact_App/NewContactWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _004_Contact_App.Classes;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+
 
 namespace _004_Contact_App
 {
@@ -27,6 +30,22 @@ namespace _004_Contact_App
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             //save contact 
+            Contact contact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text, 
+                Phone = phoneTextBox.Text
+            };
+
+            string dataBaseName = "Contact.db";
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);   
+            string dataBasePath = System.IO.Path.Combine(folderPath, dataBaseName);
+
+            SQLiteConnection connection = new SQLiteConnection(dataBasePath);
+            connection.CreateTable<Contact>();
+            connection.Insert(contact);
+            connection.Close();
+
             this.Close();
         }
     }
