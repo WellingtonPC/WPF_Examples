1d31f36d09273437aaaf5aa08eabb52a4446b5c9
Wellington Pereira da Costa <well_pcosta@hotmail.com>
2022-01-31 20:46:28 -0300

    override ToString() from Contact class to show object text

--- 004_Contact_App/Classes/Contact.cs
+++ 004_Contact_App/Classes/Contact.cs
@@ -16,5 +16,9 @@ namespace _004_Contact_App.Classes
         public string Email { get; set; }
         public string Phone { get; set; }
 
+        public override string ToString()
+        {
+            return $"{Name} - {Email} - {Phone}";
+        }
     }
 }
